440cf794d8bbfcfdf24caf2b79b19057c741f4b2
ScissorHands26 <ryan.scissons@gmail.com>
2024-05-23 19:03:00 +0000

    Update page.tsx and package.json

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,16 @@
-export default function Home() {
+import getUser from "@/pb/getUser";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const user: any = await getUser();
+
+  if (user) {
+    redirect("/c3");
+  } else {
+    redirect("/login")
+  }
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
   );
