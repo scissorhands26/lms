0b5fe477d440d1c3689755319683aeedeee846e4
ScissorHands26 <ryan.scissons@gmail.com>
2024-06-12 17:35:21 +0000

    Updated app

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,3 +1,6 @@
+import { useRouter } from "next/router";
+import { useConfig } from "nextra-theme-docs";
+
 export default {
   logo: <span>C3</span>,
   project: {
@@ -17,6 +20,32 @@ export default {
         <path d="M3,21 h18 C 21,12 3,12 3,21" />
       </svg>
     ),
+  },
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+    if (asPath !== "/") {
+      return {
+        titleTemplate: "%s – C3",
+      };
+    }
+  },
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter();
+    const { frontMatter } = useConfig();
+    const url =
+      "https://c3.cnmfu.local" +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
+
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || "Nextra"} />
+        <meta
+          property="og:description"
+          content={frontMatter.description || "Cyber Crash Course"}
+        />
+      </>
+    );
   },
   footer: {
     text: (
